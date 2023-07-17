34fc76ea76b92826d542fc812484579e44a05674
Omri Zohar <omri.zohar@datadoghq.com>
2023-07-17 17:34:26 +0300

    logging

--- install_helper.sh
+++ install_helper.sh
@@ -10,10 +10,6 @@ ddtool auth token registry --datacenter us1.ddbuild.io >> /dev/null
 if [ -f "\$HOME/.bash-git-prompt/gitprompt.sh" ]; then
     GIT_PROMPT_ONLY_IN_REPO=1
     source \$HOME/.bash-git-prompt/gitprompt.sh
-else
-    echo "Initial setup - please wait"
-    /usr/bin/git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
-    /usr/bin/vim +PlugInstall +qall
 fi
 if [[ \$- =~ i ]] && [[ -z "\$TMUX" ]] && [[ -n "\$SSH_TTY" ]]; then
   tmux attach-session -t ssh_tmux || tmux new-session -s ssh_tmux
@@ -25,5 +21,7 @@ sudo apt-get -q -y install vim tmux curl git psmisc htop
 mv ~/dotfiles/.vimrc ~/.vimrc
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
+echo "Initial setup - please wait"
+/usr/bin/git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
+/usr/bin/vim +PlugInstall +qall
 
