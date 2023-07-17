cbc9bffa0be72b505d8be683ec5e4123b91f9144
Omri Zohar <omri.zohar@datadoghq.com>
2023-07-17 10:46:37 +0300

    Update

--- install.sh
+++ install.sh
@@ -7,12 +7,12 @@ git config --global user.email "omri.zohar@datadoghq.com"
 # Updating bashrc
 git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
 cat >> ~/.bashrc <<EOL
-ddtool auth token registry --datacenter us1.ddbuild.io
-if [ -f "$HOME/.bash-git-prompt/gitprompt.sh" ]; then
+ddtool auth token registry --datacenter us1.ddbuild.io >> /dev/null
+if [ -f "\$HOME/.bash-git-prompt/gitprompt.sh" ]; then
     GIT_PROMPT_ONLY_IN_REPO=1
-    source $HOME/.bash-git-prompt/gitprompt.sh
+    source \$HOME/.bash-git-prompt/gitprompt.sh
 fi
-if [[ $- =~ i ]] && [[ -z "$TMUX" ]] && [[ -n "$SSH_TTY" ]]; then
+if [[ \$- =~ i ]] && [[ -z "\$TMUX" ]] && [[ -n "\$SSH_TTY" ]]; then
   tmux attach-session -t ssh_tmux || tmux new-session -s ssh_tmux
 fi
 EOL
