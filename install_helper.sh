93902c307516770c1d62dac5e48ec4eea6835fbf
Omri Zohar <omri.zohar@datadoghq.com>
2026-02-04 18:12:04 -0500

    removing bash stuff

--- install_helper.sh
+++ install_helper.sh
@@ -12,18 +12,6 @@ git config --global merge.conflictStyle zdiff3
 git config --global delta.side-by-side true
 
 
-# Updating bashrc
-cat >> ~/.bashrc <<EOL
-ddtool auth token registry --datacenter us1.ddbuild.io >> /dev/null
-if [ -f "\$HOME/.bash-git-prompt/gitprompt.sh" ]; then
-    GIT_PROMPT_ONLY_IN_REPO=1
-    source \$HOME/.bash-git-prompt/gitprompt.sh
-fi
-if [[ \$- =~ i ]] && [[ -z "\$TMUX" ]] && [[ -n "\$SSH_TTY" ]]; then
-  tmux attach-session -t ssh_tmux || tmux new-session -s ssh_tmux
-fi
-EOL
-
 cat >> ~/.zshrc <<EOL
 ddtool auth token registry --datacenter us1.ddbuild.io >> /dev/null
 if [[ \$- =~ i ]] && [[ -z "\$TMUX" ]] && [[ -n "\$SSH_TTY" ]]; then
@@ -42,7 +30,6 @@ sudo dpkg -i git-delta_0.18.2_amd64.deb
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 mv ~/.gitconfig ~/.gitconfig.bck
-/usr/bin/git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
 go install github.com/bazelbuild/buildtools/buildifier@latest
 go install github.com/rakyll/gotest@latest
 mv ~/.gitconfig.bck ~/.gitconfig
